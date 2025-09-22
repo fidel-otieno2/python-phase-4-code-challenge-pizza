f9353967a4acf0d6f243d8e3000834023211c33a
fidel-otieno2 <fidelotieno05@gmail.com>
2025-09-22 12:26:01 +0300

    finished code challenge

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -18,6 +18,58 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+
+class RestaurantsResource(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in restaurants], 200
+
+
+class RestaurantByIDResource(Resource):
+    def get(self, id):
+        restaurant = db.session.get(Restaurant, id)
+        if restaurant:
+            return restaurant.to_dict(), 200
+        return {"error": "Restaurant not found"}, 404
+
+    def delete(self, id):
+        restaurant = db.session.get(Restaurant, id)
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return '', 204
+        return {"error": "Restaurant not found"}, 404
+
+
+class PizzasResource(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [pizza.to_dict(only=('id', 'name', 'ingredients')) for pizza in pizzas], 200
+
+
+class RestaurantPizzasResource(Resource):
+    def post(self):
+        data = request.get_json()
+
+        try:
+            new_restaurant_pizza = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+            return new_restaurant_pizza.to_dict(), 201
+        except ValueError as e:
+            return {"errors": ["validation errors"]}, 400
+
+
+# Add routes
+api.add_resource(RestaurantsResource, '/restaurants')
+api.add_resource(RestaurantByIDResource, '/restaurants/<int:id>')
+api.add_resource(PizzasResource, '/pizzas')
+api.add_resource(RestaurantPizzasResource, '/restaurant_pizzas')
 
 
 @app.route("/")
